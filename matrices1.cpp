9a8656ababe55e7df453b9b0e6142550584c0fcc
acatarip <acatarip@unjbg.edu.pe>
2024-07-15 10:22:57 -0500

    matrices1.cpp : Se agrego codigo para determinar suma de elementos encima de la diagonal principal

--- matrices1.cpp
+++ matrices1.cpp
@@ -10,6 +10,20 @@ int main (){
             cout<<"Ingrese valor del elemento ["<<i+1<<", "<<j+1<<"]: "; cin>>A[i][j];
         }
     }
+    float SS=0;
+    cout<<"\tPara la matriz: \n";
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (j>i){
+                SS+=A[i][j];
+            }
+            cout<<A[i][j]<<" ";
+            if (j==n-1){
+                cout<<endl;
+            }
+        }
+    }
+    cout<<"La suma de los elementos por encima de su diagonal principal es "<<SS;
 
     return 0;
 }
