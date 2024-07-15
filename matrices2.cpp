075dad6729b08429e1b4e51dfb8c0bf8702c95dc
acatarip <acatarip@unjbg.edu.pe>
2024-07-15 10:47:57 -0500

    matrices2.cpp : Se agrego codigo para determinar sumatoria de la diagonal secundaria

--- matrices2.cpp
+++ matrices2.cpp
@@ -10,6 +10,19 @@ int main (){
             cout<<"Ingrese valor del elemento ["<<i+1<<", "<<j+1<<"]: "; cin>> A[i][j];
         }
     }
-
+    float SS=0;
+    cout<<"\nPara la matriz: \n";
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (i+j==n-1){
+                SS+=A[i][j];
+            }
+            cout<<A[i][j]<<" ";
+            if (j==n-1){
+                cout<<endl;
+            }
+        }
+    }
+    cout<<"La sumatoria de la diagonal secundaria es "<<SS;
     return 0;
 }
